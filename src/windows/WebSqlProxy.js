7cd2db3c75b5fad5c9c4c120f8669e0ac0c9ac77
stefanomottawr <stefano.motta@webratio.com>
2017-09-26 14:40:23 +0200

    Fix multiple connections race condition in windows
    Also fix executeSql callbacks not being asynchronous as per the spec.

--- src/windows/WebSqlProxy.js
+++ src/windows/WebSqlProxy.js
@@ -11,6 +11,8 @@ var pathSeparator = "\\";
 function generateDbPath(dbFileName) {
     return localFolderPath + pathSeparator + dbFileName;
 }
+var connectQueue = [];
+var connected = false;
 
 module.exports = {
     db: null,
@@ -84,47 +86,61 @@ module.exports = {
     executeSql: function (success, fail, args) {
         try {
             var connectionId = args.shift();
-            var res = SQLite.Proxy.SQLiteProxy.executeSql(connectionId, args);
-            res = JSON.parse(res);
+            var pr = Promise.resolve(SQLite.Proxy.SQLiteProxy.executeSql(connectionId, args));
+            pr.then(function (res) { // Using Promise for queuing callbacks into a new microtask, as the spec requires
+                res = JSON.parse(res);
 
-            // You can't access the original message text from JavaScript code.
-            // http://msdn.microsoft.com/en-US/library/windows/apps/br230301.aspx#ThrowingExceptions
-            // so we return it via custom object
-            if (res && res.message) {
-                fail(res);
-                return;
-            }
+                // You can't access the original message text from JavaScript code.
+                // http://msdn.microsoft.com/en-US/library/windows/apps/br230301.aspx#ThrowingExceptions
+                // so we return it via custom object
+                if (res && res.message) {
+                    fail(res);
+                    return;
+                }
 
-            success(res);
-        } catch(ex) {
-            fail(ex);
-        }
-    },
-
-    connect: function(success, fail, args) {
-        try {
-            var dbName = args.shift();
-            module.exports.db = generateDbPath(dbName);
-
-            var res = SQLite.Proxy.SQLiteProxy.connectToDb(module.exports.db);
-            res = JSON.parse(res);
-
-            if (res && res.message) {
-                fail(res);
-                return;
-            }
-
-            success(res);
+                success(res);
+            }).catch(function (ex) {
+                fail(ex);
+            })
         } catch (ex) {
             fail(ex);
         }
     },
 
-    disconnect: function(success, fail, connectionId) {
+    _tryConnect: function () {
+        if (!connected && connectQueue.length) {
+            var connection = connectQueue.shift();
+
+            try {
+                var dbName = connection.args.shift();
+                module.exports.db = generateDbPath(dbName);
+
+                var res = SQLite.Proxy.SQLiteProxy.connectToDb(module.exports.db);
+                res = JSON.parse(res);
+
+                if (res && res.message) {
+                    connection.fail(res);
+                    return;
+                }
+                connected = true;
+                connection.success(res);
+            } catch (ex) {
+                connection.fail(ex);
+            }
+        }
+    },
+
+    connect: function (success, fail, args) {
+        connectQueue.push({ success: success, fail: fail, args: args });
+        module.exports._tryConnect();
+    },
+
+    disconnect: function (success, fail, connectionId) {
         try {
             var res = SQLite.Proxy.SQLiteProxy.disconnect(connectionId.shift());
             res = JSON.parse(res);
-
+            connected = false;
+            setTimeout(module.exports._tryConnect, 0);
             if (res && res.message) {
                 fail(res);
                 return;
@@ -132,6 +148,8 @@ module.exports = {
 
             success();
         } catch (ex) {
+            connected = false;
+            setTimeout(module.exports._tryConnect, 0);
             fail(ex);
         }
     }
